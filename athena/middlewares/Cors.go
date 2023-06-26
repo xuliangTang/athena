9ea3bf65be32acf4a3ce938a6ddda561987af90c
xuliangTang <1297441127@qq.com>
2023-06-26 11:31:45 +0800

    fix: cors add x-token header

--- athena/middlewares/Cors.go
+++ athena/middlewares/Cors.go
@@ -18,7 +18,7 @@ func (*Cors) OnRequest(ctx *gin.Context) {
 	if origin != "" {
 		ctx.Header("Access-Control-Allow-Origin", "*") // 可将将 * 替换为指定的域名
 		ctx.Header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, UPDATE, PATCH")
-		ctx.Header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization")
+		ctx.Header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, X-Token")
 		ctx.Header("Access-Control-Expose-Headers", "Content-Length, Access-Control-Allow-Origin, Access-Control-Allow-Headers, Cache-Control, Content-Language, Content-Type")
 		ctx.Header("Access-Control-Allow-Credentials", "true")
 	}
