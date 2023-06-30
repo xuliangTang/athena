e6a190851e38c5089ef490b5a42814eb3232009c
xuliangTang <1297441127@qq.com>
2023-06-30 18:36:34 +0800

    feat: dbQuery对象

--- athena/Page.go
+++ athena/Page.go
@@ -7,19 +7,123 @@ import (
 	"strconv"
 )
 
+// DbQuery 查询对象
+type DbQuery struct {
+	Db       *gorm.DB    `json:"-"`
+	Debug    bool        `json:"-"`
+	Order    string      `json:"-"`
+	Fields   []string    `json:"-"`
+	Preloads []*Preload  `json:"-"`
+	Joins    []*Join     `json:"-"`
+	Where    *Conditions `json:"-"`
+}
+
+func NewDbQuery() *DbQuery {
+	return &DbQuery{}
+}
+
+// SetDb 设置db对象
+func (this *DbQuery) SetDb(db *gorm.DB) *DbQuery {
+	this.Db = db
+	return this
+}
+
+// SetDb 设置Debug
+func (this *DbQuery) SetDebug() *DbQuery {
+	this.Debug = true
+	return this
+}
+
+// SetWhere 设置查询条件
+func (this *DbQuery) SetWhere(where *Conditions) *DbQuery {
+	this.Where = where
+	return this
+}
+
+// SetOrder 设置排序
+func (this *DbQuery) SetOrder(order string) *DbQuery {
+	this.Order = order
+	return this
+}
+
+// AddPreloads 设置预加载
+func (this *DbQuery) AddPreloads(preloads ...*Preload) *DbQuery {
+	this.Preloads = append(this.Preloads, preloads...)
+	return this
+}
+
+// AddJoins 设置关联
+func (this *DbQuery) AddJoins(joins ...*Join) *DbQuery {
+	this.Joins = append(this.Joins, joins...)
+	return this
+}
+
+// AddFields 设置查询字段
+func (this *DbQuery) AddFields(fields ...string) *DbQuery {
+	this.Fields = append(this.Fields, fields...)
+	return this
+}
+
+// SetCountBuildOpts 设置查询选项
+func (this *DbQuery) SetBuildOpts(build *gorm.DB) *gorm.DB {
+	if this.Debug {
+		build = build.Debug()
+	}
+
+	if len(this.Fields) > 0 {
+		build = build.Select(this.Fields)
+	}
+
+	if this.Order != "" {
+		build = build.Order(this.Order)
+	}
+
+	if len(this.Preloads) > 0 {
+		for _, p := range this.Preloads {
+			build = build.Preload(p.Query, p.Args...)
+		}
+	}
+
+	if len(this.Joins) > 0 {
+		for _, j := range this.Joins {
+			build = build.Joins(j.Query, j.Args...)
+		}
+	}
+
+	if this.Where != nil {
+		build = build.Where(this.Where.Query, this.Where.Args...)
+	}
+
+	return build
+}
+
+// SetCountBuildOpts 设置统计数量的查询选项
+func (this *DbQuery) SetCountBuildOpts(countBuild *gorm.DB) *gorm.DB {
+	if this.Debug {
+		countBuild = countBuild.Debug()
+	}
+
+	if len(this.Joins) > 0 {
+		for _, j := range this.Joins {
+			countBuild = countBuild.Joins(j.Query, j.Args...)
+		}
+	}
+
+	if this.Where != nil {
+		countBuild = countBuild.Where(this.Where.Query, this.Where.Args...)
+	}
+
+	return countBuild
+}
+
+// Page 快速分页对象
 type Page struct {
-	Db          *gorm.DB    `json:"-"`
-	Debug       bool        `json:"-"`
-	CurrentPage int         `json:"current_page"`     // 当前页
-	PerPage     int         `json:"per_page"`         // 每页条数
-	TotalSize   int64       `json:"total_size"`       // 总条数
-	TotalPage   int         `json:"total_page"`       // 总页数
-	Extend      any         `json:"extend,omitempty"` // 扩展字段
-	Order       string      `json:"-"`
-	Fields      []string    `json:"-"`
-	Preloads    []*Preload  `json:"-"`
-	Joins       []*Join     `json:"-"`
-	Where       *Conditions `json:"-"`
+	*DbQuery
+	CurrentPage int   `json:"current_page"`     // 当前页
+	PerPage     int   `json:"per_page"`         // 每页条数
+	TotalSize   int64 `json:"total_size"`       // 总条数
+	TotalPage   int   `json:"total_page"`       // 总页数
+	Extend      any   `json:"extend,omitempty"` // 扩展字段
 }
 
 func NewPage(currentPage int, perPage int) *Page {
@@ -45,49 +149,7 @@ func NewPageWithCtx(ctx *gin.Context) *Page {
 		getOrder = "id DESC"
 	}
 
-	return &Page{CurrentPage: currentPage, PerPage: perPage, Order: getOrder}
-}
-
-// SetDb 设置db对象
-func (this *Page) SetDb(db *gorm.DB) *Page {
-	this.Db = db
-	return this
-}
-
-// SetDb 设置Debug
-func (this *Page) SetDebug() *Page {
-	this.Debug = true
-	return this
-}
-
-// SetWhere 设置查询条件
-func (this *Page) SetWhere(where *Conditions) *Page {
-	this.Where = where
-	return this
-}
-
-// SetOrder 设置排序
-func (this *Page) SetOrder(order string) *Page {
-	this.Order = order
-	return this
-}
-
-// AddPreloads 设置预加载
-func (this *Page) AddPreloads(preloads ...*Preload) *Page {
-	this.Preloads = append(this.Preloads, preloads...)
-	return this
-}
-
-// AddJoins 设置关联
-func (this *Page) AddJoins(joins ...*Join) *Page {
-	this.Joins = append(this.Joins, joins...)
-	return this
-}
-
-// AddFields 设置查询字段
-func (this *Page) AddFields(fields ...string) *Page {
-	this.Fields = append(this.Fields, fields...)
-	return this
+	return &Page{CurrentPage: currentPage, PerPage: perPage, DbQuery: &DbQuery{Order: getOrder}}
 }
 
 // IsValid 是否有效
@@ -111,39 +173,14 @@ func (this *Page) SelectList(items any) *gorm.DB {
 	build := this.Db.Limit(this.PerPage).Offset(this.GetOffset())
 	buildCount := this.Db.Model(items)
 
-	if this.Debug {
-		build = build.Debug()
-		buildCount = buildCount.Debug()
-	}
+	// 设置query参数
+	build = this.SetBuildOpts(build)
+	buildCount = this.SetCountBuildOpts(buildCount)
 
-	if len(this.Fields) > 0 {
-		build = build.Select(this.Fields)
-	}
-
-	if this.Order != "" {
-		build = build.Order(this.Order)
-	}
-
-	if len(this.Preloads) > 0 {
-		for _, p := range this.Preloads {
-			build = build.Preload(p.Query, p.Args...)
-		}
-	}
-
-	if len(this.Joins) > 0 {
-		for _, j := range this.Joins {
-			build = build.Joins(j.Query, j.Args...)
-			buildCount = buildCount.Joins(j.Query, j.Args...)
-		}
-	}
-
-	if this.Where != nil {
-		build = build.Where(this.Where.Query, this.Where.Args...)
-		buildCount = buildCount.Where(this.Where.Query, this.Where.Args...)
-	}
-
+	// 查询
 	tx := build.Find(items)
 
+	// 统计总数
 	var totalSize int64
 	buildCount.Count(&totalSize)
 	this.SetTotal(totalSize)
